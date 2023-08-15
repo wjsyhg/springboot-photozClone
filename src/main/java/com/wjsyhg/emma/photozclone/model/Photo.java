b01b80044c9ddc22507a181a0fcb9b2f0433d3fd
wjsyhg <wjsyhg@gmail.com>
2023-08-15 17:53:31 +0100

    Initial commit

---
+++ src/main/java/com/wjsyhg/emma/photozclone/model/Photo.java
@@ -0,0 +1,60 @@
+package com.wjsyhg.emma.photozclone.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotEmpty;
+
+
+
+public class Photo {
+    private String id;
+    @NotEmpty
+    private String fileName;
+
+    private String contentType;
+
+    @JsonIgnore
+    private byte[] data;
+
+
+
+    public Photo() {
+
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
---
+++ src/main/java/com/wjsyhg/emma/photozclone/web/PhotozController.java
@@ -0,0 +1,53 @@
+package com.wjsyhg.emma.photozclone.web;
+
+import com.wjsyhg.emma.photozclone.model.Photo;
+import com.wjsyhg.emma.photozclone.service.PhotozService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.UUID;
+
+@RestController
+public class PhotozController {
+
+    private final PhotozService photozService;
+
+    public PhotozController(PhotozService photozService) {
+        this.photozService = photozService;
+    }
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/photoz")
+    public Collection<Photo> get() {
+        return photozService.get();
+    }
+
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = photozService.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = photozService.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        return  photozService.save(file.getOriginalFilename(), file.getContentType(), file.getBytes());
+    }
+
+
+}
